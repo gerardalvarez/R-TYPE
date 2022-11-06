110b2d9dd0abaa30f2019552ada2b03297f1033e
Gerard <77858157+gerardalvarez@users.noreply.github.com>
2022-11-06 22:05:30 +0100

    menu

--- 2DGame/02-Bubble/02-Bubble/MenuScene.cpp
+++ 2DGame/02-Bubble/02-Bubble/MenuScene.cpp
@@ -5,6 +5,12 @@
 #include "Game.h"
 
 
+
+enum MenuAnims
+{
+	NORMAL1
+};
+
 MenuScene::MenuScene() {}
 MenuScene:: ~MenuScene() {}
 
@@ -14,18 +20,29 @@ void MenuScene::init(int level) {
 	switch (level) {
 	case (0): //MENU
 		spritesheet.loadFromFile("images/r-menu.png", TEXTURE_PIXEL_FORMAT_RGB);
+		background = Sprite::createSprite(glm::ivec2(256, 192), glm::vec2(1280 / 1280.f, 720 / 1444.f), &spritesheet, &texProgram);
+		background->setNumberAnimations(1);
+		background->setAnimationSpeed(NORMAL1, 8);
+		background->addKeyframe(NORMAL1, glm::vec2(0 / 1280.f, 720 / 1444.f));
+		background->addKeyframe(NORMAL1, glm::vec2(0 / 1280.f, 0 / 1444.f));
+		
+		background->changeAnimation(NORMAL1);
+		background->setLoopAnimations(true);
 		break;
 	case(1): //CREDITS
 		spritesheet.loadFromFile("images/r-creditos.png", TEXTURE_PIXEL_FORMAT_RGBA);
+		background = Sprite::createSprite(glm::ivec2(256, 192), glm::vec2(1.f, 1.f), &spritesheet, &texProgram);
+		background->setPosition(glm::vec2(0.f, 0.f));
 		break;
 	case(2): //CREDITS
 		spritesheet.loadFromFile("images/r-instr.png", TEXTURE_PIXEL_FORMAT_RGBA);
+		background = Sprite::createSprite(glm::ivec2(256, 192), glm::vec2(1.f, 1.f), &spritesheet, &texProgram);
+		background->setPosition(glm::vec2(0.f, 0.f));
 		break;
 
 	}
 	//spritesheet.loadFromFile("images/Menu_extra.png", TEXTURE_PIXEL_FORMAT_RGBA);
-	background = Sprite::createSprite(glm::ivec2(256, 192), glm::vec2(1.f, 1.f), &spritesheet, &texProgram);
-	background->setPosition(glm::vec2(0.f, 0.f));
+	
 
 	projection = glm::ortho(0.f, float(SCREEN_WIDTH - 1), float(SCREEN_HEIGHT - 1), 0.f);
 
--- 2DGame/02-Bubble/02-Bubble/MenuScene.h
+++ 2DGame/02-Bubble/02-Bubble/MenuScene.h
@@ -21,4 +21,5 @@ public:
 private:
 	Texture scene;
 	Text text;
+	Sprite* sprite;
 };
